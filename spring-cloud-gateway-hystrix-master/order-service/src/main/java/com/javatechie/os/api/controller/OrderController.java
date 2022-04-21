ca448ddb1978bd77a45ab56f051bb6a48d28bf83
vietlv2 <vietlv2@neo.vn>
2022-04-21 11:18:15 +0700

    init

---
+++ spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.javatechie.os.api.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.javatechie.os.api.common.Payment;
+import com.javatechie.os.api.common.TransactionRequest;
+import com.javatechie.os.api.common.TransactionResponse;
+import com.javatechie.os.api.entity.Order;
+import com.javatechie.os.api.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+    
+    @PostMapping("/bookOrder")
+    public TransactionResponse bookOrder(@RequestBody TransactionRequest request) throws JsonProcessingException {
+        return service.saveOrder(request);
+    }
+
+    @GetMapping("/get")
+    public String get(){
+        return "Success";
+    }
+}
